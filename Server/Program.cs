17ca599c2b536ca6163237e5d36535ae6186f4f9
Daria <shkatov.pad@gmail.com>
2023-08-14 22:33:56 +0300

    dfds

---
+++ Server/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("This is our server");
+            OurServer server = new OurServer();
+        }
+    }
+}
